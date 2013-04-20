efec52b8ee41ba8ea3d0a56266a80fd9d31bd5b0
parkh <da.parho@gmail.com>
2013-04-20 11:04:17 +0300

    one more source added

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -3,60 +3,71 @@ class SourcesController < ApplicationController
   require 'mechanize'
 
   def search
-    city = params[:search]
-    validate(city)
-  end
+    city_name = params[:search]
 
-  def validate(city)
     agent = Mechanize.new
     agent.get("http://sinoptik.ua")
     form = agent.page.forms.first
-    form.search_city = city
+    form.search_city = city_name
     form.submit
     if CGI::unescape(agent.page.uri.to_s) =~ /погода-/ && CGI::unescape(agent.page.uri.to_s) !~ /search_city/
-      parse(agent)
+      if params[:commit] == "sinoptik"
+        parse_sinoptik(city_name)
+      elsif params[:commit] == "meteo"
+        parse_meteo(city_name)
+      end
     else
-      flash[:notice] = "Город #{city} еще не построили, либо Sinoptik.ua не знает о нем."
+      flash[:notice] = "Город #{city_name} еще не построили, либо ни Sinoptik.ua, ни Meteo.ua пока не знают о нем."
       redirect_to controller: 'main', action: 'index'
     end
   end
 
-  def parse(agent)
+  def parse_sinoptik(city_name)
+    agent = Mechanize.new
+    agent.get("http://sinoptik.ua")
+    form = agent.page.forms.first
+    form.search_city = city_name
+    form.submit
+
     city = City.new
     city.name = agent.page.search(".cityNameShort strong").text
     city.min_t = agent.page.search(".min span").text.split("°")
     city.max_t = agent.page.search(".max span").text.split("°")
     city.user_id = session[:user_id]
 
-    week_chance = []
-    agent.page.links_with(href: /2013/).each do |link|
-      link.click
-      day_chance = []
-      agent.page.search("tr:nth-child(8) td").each do |chance|
-        day_chance << chance.text.to_i
-      end
-    week_chance << day_chance.max
-    end
-    city.precip_chance = week_chance
+    save(city)
+  end
 
+  def parse_meteo(city_name)
+    agent = Mechanize.new
+    agent.get("http://meteo.ua")
+    form = agent.page.form_with(id: "search-form")
+    form.field.value = city_name
+    form.submit
+
+    if CGI::unescape(agent.page.uri.to_s) =~ /search-forecast-by-city-name/
+      page = agent.page
+      node = page.search(".main_cont p:nth-child(2) a").first
+      Mechanize::Page::Link.new(node, agent, page).click
+    end
+
+    city = City.new
+    city.name = agent.page.search(".src_title .txt_pink").text
+    city.min_t = agent.page.search(".wwt_min span:nth-child(2)").text.split("°")
+    city.max_t = agent.page.search(".wwt_max span:nth-child(2)").text.split("°")
+    city.user_id = session[:user_id]
+
+    save(city)
+  end
+
+  def save(city)
     if city.save
       flash[:notice] = "Город найден и добавлен в базу данных."
       redirect_to controller: 'main', action: 'index'
     else
-      flash[:notice] = "С названием города что-то не так. Нужен баг репорт."
+      flash[:notice] = "Парсируемый сайт выдал что-то непредсказуемое..."
       redirect_to controller: 'main', action: 'index'
     end
   end
-
-#  def bigmir
-#    agent = Mechanize.new
-#    agent.get("http://weather.bigmir.net/")
-#    form = agent.page.forms.first
-#    form.q = "Черкассы"
-#    form.submit
-#    agent.page.links_with(text: /ерка/ )[0].click
-#    agent.page.links_with(href: /forecast/ )[0].click
-#    @bigmir = agent.page.search("td:nth-child(3) b").text.split("%")
-#  end
 
 end
